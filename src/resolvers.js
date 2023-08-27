2b608821138c3d739506f7615d02563ede0ebeba
mouna <mouna.khila@aleriaconsulting.eu>
2023-08-27 13:25:47 +0100

    changes in the id type

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,7 +1,17 @@
 import { users } from "./db";
+
 const resolvers = {
   Query: {
-    hello: () => "Hello GraphQl",
+    user: (parent, { id }, context, info) => {
+      console.log(`Querying user with id: ${id}`);
+      const user = users.find((user) => user.id === id);
+      console.log("User found:", user);
+      return user;
+    },
+    users: (parent, args, context, info) => {
+      return users;
+    },
   },
 };
+
 export default resolvers;
